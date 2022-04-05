1a91f4df355ddab6e9d754c8e1443315d82a7c7b
foodstore-duynd <102868184+foodstore-duynd@users.noreply.github.com>
2022-04-05 10:28:57 +0700

    Update RestaurantApplication.java

--- src/main/java/com/restarant/backend/RestaurantApplication.java
+++ src/main/java/com/restarant/backend/RestaurantApplication.java
@@ -25,6 +25,7 @@ public class RestaurantApplication {
         // done know
         ///vasldas
         //h lleoor
+        // blabla
     }
 
 //    private static void logApplicationStartup(Environment env) {

dea6435129cd90849ef9c210293befe95fa635eb
foodstore-duynd <102868184+foodstore-duynd@users.noreply.github.com>
2022-04-05 10:16:08 +0700

    Update RestaurantApplication.java

--- src/main/java/com/restarant/backend/RestaurantApplication.java
+++ src/main/java/com/restarant/backend/RestaurantApplication.java
@@ -21,7 +21,7 @@ public class RestaurantApplication {
         SpringApplication app = new SpringApplication(RestaurantApplication.class);
         Environment env = app.run(args).getEnvironment();
         //logApplicationStartup(env);
-        //"klog dasllds
+        //"klog dasllds fuckoff
         ///vasldas
     }
 
